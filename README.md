f67b0921c3fd7927782eada48da77ae08732a04a
Arnaldo Liberal dos Santos <naldo@MacBook-Pro-de-Arnaldo.local>
2019-02-20 12:06:00 +0100

    Finally

---
+++ README.md
@@ -0,0 +1,3 @@
+USE:
+NodeJs
+MongoDB
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'XXXXXXXXXXXXXXXXXX';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, texto da mensgaem.!';
+
+module.exports = {
+    connectionString: 'XXXXXXXXXXXXXXXXXXXX',
+    sendgridKey: 'XXXXXXXXX',
+    containerConnectionString: 'XXXXXXXXXXX'
+}
